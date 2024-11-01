e90fb4b80e133ef5fc8f0cb9c9cc01947ad6e50e
afrzlfa <afrzlf4@gmail.com>
2024-11-01 14:12:53 +0700

    Update

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 echo "[*] [Running]"
 
 mkdir -p sonarqube_postgres sonarqube
-mkdir -p pgdata sonarqube_data
 
 cat <<EOF > sonarqube_postgres/Dockerfile
 FROM postgres:latest
@@ -13,8 +12,6 @@ ENV POSTGRES_PASSWORD=sonar
 ENV POSTGRES_DB=sonar
 
 EXPOSE 5432
-
-VOLUME /var/lib/postgresql/data
 EOF
 
 cat <<EOF > sonarqube/Dockerfile
@@ -25,9 +22,6 @@ ENV SONAR_JDBC_USERNAME=sonar
 ENV SONAR_JDBC_PASSWORD=sonar
 
 EXPOSE 9000
-
-VOLUME /opt/sonarqube/data
-VOLUME /opt/sonarqube/extensions
 EOF
 
 if [ "$(docker ps -aq -f name=sonar-postgres)" ]; then
@@ -49,14 +43,14 @@ if [ "$(docker images -q sonarqube 2> /dev/null)" ]; then
 fi
 
 docker build -t sonar-postgres ./sonarqube_postgres
-docker run -d --name sonar-postgres -p 5432:5432 -v $(pwd)/pgdata:/var/lib/postgresql/data sonar-postgres
+docker run -d --name sonar-postgres -p 5432:5432 sonar-postgres
 
 until [ "$(docker logs sonar-postgres 2>&1 | grep 'database system is ready to accept connections')" ]; do
     sleep 1
 done
 
 docker build -t sonarqube ./sonarqube
-docker run -d --name sonarqube -p 9000:9000 -v $(pwd)/sonarqube_data:/opt/sonarqube/data --link sonar-postgres:postgres sonarqube
+docker run -d --name sonarqube -p 9000:9000 --link sonar-postgres:postgres sonarqube
 
 echo "[+] [SonarQube URL: http://localhost:9000]"
 echo "[+] [Username: admin]"
